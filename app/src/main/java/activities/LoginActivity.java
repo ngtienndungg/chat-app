cccb88b667b1db7c8bf6b4a4bc5acdd2542afcca
ngtienndungg <dungnt.works@gmail.com>
2023-07-16 23:07:59 +0700

    setup data binding

--- app/src/main/java/activities/LoginActivity.java
+++ app/src/main/java/activities/LoginActivity.java
@@ -1,16 +1,27 @@
 package activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 
-import com.example.chat.R;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.chat.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private ActivityLoginBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        eventHandling();
+    }
+
+    private void eventHandling() {
+        binding.activityLoginTvCreateAccount.setOnClickListener(v ->
+                startActivity(new Intent(getApplicationContext(), RegisterActivity.class)));
     }
 }
--- app/src/main/java/activities/RegisterActivity.java
+++ app/src/main/java/activities/RegisterActivity.java
@@ -3,14 +3,26 @@ package activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.example.chat.R;
+import com.example.chat.databinding.ActivityLoginBinding;
+import com.example.chat.databinding.ActivityRegisterBinding;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private ActivityRegisterBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        binding = ActivityRegisterBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        eventHandling();
+    }
+
+    private void eventHandling() {
+        binding.activityRegisterBtLogin.setOnClickListener(v -> onBackPressed());
     }
 }
