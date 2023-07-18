c52aff1a9108a84bab8f390e5143885b0e88d3ab
ngtienndungg <dungnt.works@gmail.com>
2023-07-18 10:42:56 +0700

    handle exceptions on register activity

--- app/src/main/java/com/example/chat/activities/RegisterActivity.java
+++ app/src/main/java/com/example/chat/activities/RegisterActivity.java
@@ -9,6 +9,7 @@ import android.provider.MediaStore;
 import android.util.Base64;
 import android.util.Patterns;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.activity.result.ActivityResultLauncher;
@@ -18,11 +19,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.chat.R;
 import com.example.chat.databinding.ActivityRegisterBinding;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
 import com.google.firebase.auth.FirebaseUser;
 
 import java.io.ByteArrayOutputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.util.Objects;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -68,6 +71,11 @@ public class RegisterActivity extends AppCompatActivity {
         });
     }
 
+    private void displayError(EditText viewDisplay, String message) {
+        viewDisplay.setError(message);
+        viewDisplay.requestFocus();
+    }
+
     private void signUp() {
         if (isValidInformation()) {
             loading(true);
@@ -77,14 +85,21 @@ public class RegisterActivity extends AppCompatActivity {
                     .addOnCompleteListener(this, createTask -> {
                         if (createTask.isSuccessful()) {
                             FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-                            assert user != null;
-                            user.sendEmailVerification().addOnCompleteListener(sendTask -> {
+                            Objects.requireNonNull(user).sendEmailVerification().addOnCompleteListener(sendTask -> {
                                 loading(false);
                                 startActivity(new Intent(RegisterActivity.this, NotificationActivity.class));
                             });
                         } else {
-                            Toast.makeText(this, getString(R.string.toast_something_wrong), Toast.LENGTH_SHORT).show();
                             loading(false);
+                            String errorCode = ((FirebaseAuthException) Objects.requireNonNull(createTask.getException())).getErrorCode();
+                            switch (errorCode) {
+                                case "ERROR_EMAIL_ALREADY_IN_USE":
+                                    displayError(binding.activityRegisterEtInputEmail, getString(R.string.set_error_email_already_use));
+                                    break;
+                                case "ERROR_WEAK_PASSWORD":
+                                    displayError(binding.activityRegisterEtInputPassword, getString(R.string.set_error_weak_password));
+                                    break;
+                            }
                         }
                     });
         }
@@ -100,31 +115,27 @@ public class RegisterActivity extends AppCompatActivity {
         return Base64.encodeToString(bytes, Base64.DEFAULT);
     }
 
-    private void displayToast(String message) {
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
-    }
-
     private Boolean isValidInformation() {
         if (encodedImage == null) {
-            displayToast(getString(R.string.toast_empty_profile_image));
+            Toast.makeText(this, getString(R.string.toast_empty_profile_image), Toast.LENGTH_SHORT).show();
             return false;
         } else if (binding.activityRegisterEtInputName.getText().toString().trim().isEmpty()) {
-            displayToast(getString(R.string.toast_empty_name));
+            displayError(binding.activityRegisterEtInputName, getString(R.string.set_error_empty_name));
             return false;
         } else if (binding.activityRegisterEtInputEmail.getText().toString().trim().isEmpty()) {
-            displayToast(getString(R.string.toast_empty_email));
+            displayError(binding.activityRegisterEtInputEmail, getString(R.string.set_error_empty_email));
             return false;
         } else if (!Patterns.EMAIL_ADDRESS.matcher(binding.activityRegisterEtInputEmail.getText().toString().trim()).matches()) {
-            displayToast(getString(R.string.toast_invalid_email));
+            displayError(binding.activityRegisterEtInputEmail, getString(R.string.set_error_invalid_email));
             return false;
         } else if (binding.activityRegisterEtInputPassword.getText().toString().trim().isEmpty()) {
-            displayToast(getString(R.string.toast_empty_password));
+            displayError(binding.activityRegisterEtInputPassword, getString(R.string.set_error_empty_password));
             return false;
         } else if (binding.activityRegisterEtInputConfirmPassword.getText().toString().trim().isEmpty()) {
-            displayToast(getString(R.string.toast_empty_confirm_password));
+            displayError(binding.activityRegisterEtInputConfirmPassword, getString(R.string.set_error_empty_confirm_password));
             return false;
         } else if (!binding.activityRegisterEtInputPassword.getText().toString().equals(binding.activityRegisterEtInputConfirmPassword.getText().toString())) {
-            displayToast(getString(R.string.toast_not_match_password));
+            displayError(binding.activityRegisterEtInputConfirmPassword, getString(R.string.set_error_not_match_password));
             return false;
         }
         return true;
