f759fb03c6513750a669969ca12c51fe603e1296
ngtienndungg <dungnt.works@gmail.com>
2023-07-22 21:25:34 +0700

    create preference manager

---
+++ app/src/main/java/com/example/chat/utilities/PreferenceManager.java
@@ -0,0 +1,27 @@
+package com.example.chat.utilities;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class PreferenceManager {
+    private final SharedPreferences sharedPreferences;
+    public PreferenceManager(Context context) {
+        sharedPreferences = context.getSharedPreferences(Constants.KEY_PREFERENCE_NAME, Context.MODE_PRIVATE);
+    }
+
+    public void putData(String key, String value) {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(key, value);
+        editor.apply();
+    }
+
+    public String getData(String key) {
+        return sharedPreferences.getString(key, null);
+    }
+
+    public void clear() {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.clear();
+        editor.apply();
+    }
+}
