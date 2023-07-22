29e28488b65a1dc5511158d338e337b6ece87bb1
ngtienndungg <dungnt.works@gmail.com>
2023-07-22 22:11:29 +0700

    handle display user profile

--- app/src/main/java/com/example/chat/activities/MainActivity.java
+++ app/src/main/java/com/example/chat/activities/MainActivity.java
@@ -1,27 +1,35 @@
 package com.example.chat.activities;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.util.Base64;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.chat.R;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+import com.example.chat.databinding.ActivityMainBinding;
+import com.example.chat.utilities.Constants;
+import com.example.chat.utilities.PreferenceManager;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView name;
+    private ActivityMainBinding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        name = findViewById(R.id.name);
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        if (user != null) {
-            name.setText(user.getEmail());
-        }
+        displayUserProfile();
+    }
+
+    private void displayUserProfile() {
+        PreferenceManager preferenceManager = new PreferenceManager(this);
+        binding.activityMainTvName.setText(preferenceManager.getData(Constants.KEY_NAME));
+        byte[] bytes = Base64.decode(preferenceManager.getData(Constants.KEY_IMAGE), Base64.DEFAULT);
+        Bitmap image = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+        binding.activityMainRivProfile.setImageBitmap(image);
     }
 }
