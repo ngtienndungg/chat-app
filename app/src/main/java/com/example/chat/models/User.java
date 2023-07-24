3f0a2fb85467d409710c81e191628e02ef2c003d
ngtienndungg <dungnt.works@gmail.com>
2023-07-24 17:18:42 +0700

    design user item container

---
+++ app/src/main/java/com/example/chat/models/User.java
@@ -0,0 +1,46 @@
+package com.example.chat.models;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    private String name, email, image, fcmToken;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getFcmToken() {
+        return fcmToken;
+    }
+
+    public void setFcmToken(String fcmToken) {
+        this.fcmToken = fcmToken;
+    }
+
+    public User(String name, String email, String image, String fcmToken) {
+        this.name = name;
+        this.email = email;
+        this.image = image;
+        this.fcmToken = fcmToken;
+    }
+}
